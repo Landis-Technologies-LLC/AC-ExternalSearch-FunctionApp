fb9d7ab67fbf721cf8ad3189fc7feacfec068af1
M. Stoltzfus <contact@mstoltzfus.com>
2021-12-29 14:28:48 -0500

    changed referenced http request property from "Search" to "searchTerm" as per latest AC alpha update

--- searchEndpoint/index.ts
+++ searchEndpoint/index.ts
@@ -2,9 +2,9 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import data from "./db"
 
 const httpTrigger: AzureFunction = async function ( context: Context, req: HttpRequest ): Promise<void> {
-    context.log( 'HTTP trigger function processed a request. Search Term was ' + req.body.Search );
+    context.log( 'HTTP trigger function processed a request. Search Term was ' + req.body.searchTerm );
     const responseMessage = data
-    const responseMessageNew = filterSearch( req.body.Search )
+    const responseMessageNew = filterSearch( req.body.searchTerm )
 
     context.res = {
         // status: 200, /* Defaults to 200 */
