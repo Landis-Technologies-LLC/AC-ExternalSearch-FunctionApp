d4e0b4660453761414dbf695cedf746aea587f38
M. Stoltzfus <contact@mstoltzfus.com>
2022-06-02 14:55:02 -0400

    maxResults addition

--- searchEndpoint/index.ts
+++ searchEndpoint/index.ts
@@ -2,8 +2,14 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import * as data from './contactlist.sample.json';
 
 const httpTrigger: AzureFunction = async function ( context: Context, req: HttpRequest ): Promise<void> {
-    context.log( 'HTTP trigger function processed a request. Search Term was ' + req.body.searchTerm );
-    const response = filterSearch( req.body.searchTerm )
+
+    
+    const searchTerm = req.body.searchTerm
+    const maxResults = req.body.maxContactSearchResults
+
+    context.log( 'HTTP trigger function processed a request. Search Term was ' + searchTerm + ' and max results was ' + maxResults );
+
+    const response = filterSearch( searchTerm, maxResults )
 
     context.res = {
         // status: 200, /* Defaults to 200 */
@@ -12,9 +18,10 @@ const httpTrigger: AzureFunction = async function ( context: Context, req: HttpR
 
 };
 
-function filterSearch( searchTerm: string ) {
+function filterSearch( searchTerm: string, maxResults: number ) {
     // @ts-ignore
     return data.filter( item => item.displayName.toLowerCase().includes( searchTerm.toLowerCase() ) || item.userPrincipalName.toLowerCase().includes( searchTerm.toLowerCase() ) );
 };
+
 
 export default httpTrigger;
