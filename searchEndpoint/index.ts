d009ca23858ddadf013d19a8afba08bfc233ef27
M. Stoltzfus <contact@mstoltzfus.com>
2022-06-02 17:10:06 -0400

    added types

--- searchEndpoint/index.ts
+++ searchEndpoint/index.ts
@@ -3,8 +3,8 @@ import * as data from './contactlist.sample.json';
 
 const httpTrigger: AzureFunction = async function ( context: Context, req: HttpRequest ): Promise<void> {
     
-    const searchTerm = req.body.searchTerm
-    const maxResults = req.body.maxContactSearchResults
+    const searchTerm: string = req.body.searchTerm
+    const maxResults: number = req.body.maxContactSearchResults
 
     context.log( 'HTTP trigger function processed a request. Search Term was ' + searchTerm + ' and max results was ' + maxResults );
 
